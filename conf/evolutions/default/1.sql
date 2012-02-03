f273c8e6684005394183304dcb718d1778e80251
James Ward <james@jamesward.org>
2012-02-02 19:29:54 -0700

    do not use a table named user

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -21,35 +21,35 @@ create table task (
   constraint pk_task primary key (id))
 ;
 
-create table user (
+create table account (
   email                     varchar(255) not null,
   name                      varchar(255),
   password                  varchar(255),
-  constraint pk_user primary key (email))
+  constraint pk_account primary key (email))
 ;
 
 
-create table project_user (
+create table project_account (
   project_id                     bigint not null,
-  user_email                     varchar(255) not null,
-  constraint pk_project_user primary key (project_id, user_email))
+  account_email                  varchar(255) not null,
+  constraint pk_project_account primary key (project_id, account_email))
 ;
 create sequence project_seq;
 
 create sequence task_seq;
 
-create sequence user_seq;
+create sequence account_seq;
 
-alter table task add constraint fk_task_assignedTo_1 foreign key (assigned_to_email) references user (email) on delete restrict on update restrict;
+alter table task add constraint fk_task_assignedTo_1 foreign key (assigned_to_email) references account (email) on delete restrict on update restrict;
 create index ix_task_assignedTo_1 on task (assigned_to_email);
 alter table task add constraint fk_task_project_2 foreign key (project_id) references project (id) on delete restrict on update restrict;
 create index ix_task_project_2 on task (project_id);
 
 
 
-alter table project_user add constraint fk_project_user_project_01 foreign key (project_id) references project (id) on delete restrict on update restrict;
+alter table project_account add constraint fk_project_account_project_01 foreign key (project_id) references project (id) on delete restrict on update restrict;
 
-alter table project_user add constraint fk_project_user_user_02 foreign key (user_email) references user (email) on delete restrict on update restrict;
+alter table project_account add constraint fk_project_account_account_02 foreign key (account_email) references account (email) on delete restrict on update restrict;
 
 # --- !Downs
 
@@ -57,11 +57,11 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists project;
 
-drop table if exists project_user;
+drop table if exists project_account;
 
 drop table if exists task;
 
-drop table if exists user;
+drop table if exists account;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
@@ -69,5 +69,5 @@ drop sequence if exists project_seq;
 
 drop sequence if exists task_seq;
 
-drop sequence if exists user_seq;
+drop sequence if exists account_seq;
 
